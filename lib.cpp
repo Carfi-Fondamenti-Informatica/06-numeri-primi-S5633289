531c0d22ff5a8dc6cbaf29e51f94123e3541effb
S5633289 <118168182+S5633289@users.noreply.github.com>
2022-12-02 12:29:48 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,19 @@
-
+int b=1;
+bool intero(int a){
+    bool ris = true;
+    if(a>1){
+        if(a>b+1){
+            if(a%(a-b)==0){ris=false; return ris;}
+            else{b++;ris=intero(a);return ris;}
+        }else{return true;}
+    }else{return false;}
+}
+bool intero(int a,int a2){
+    bool ris = true;
+    if(a>1&&a2>1){
+        if(a>b+1&&a2>b+1){
+            if(a%(a-b)==0||a2%(a2-b)==0){ris=false; return ris;}
+            else{b++;ris=intero(a,a2);return ris;}
+        }else{return true;}
+    }else{return false;}
+}
